1a1d69cc09e77156389ea959fe5c0dee0fd41fad
Silvan Moura <silvancortesmoura@outlook.com>
2023-05-04 03:24:20 -0300

    LogOut Created

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,34 +1,68 @@
-<template>
+<!-- <template>
     <nav>
+            <router-link to="/">Home</router-link>
         <router-link to="/">Home</router-link>
         <router-link to="/about">Sobre Nós</router-link>
+    </nav>
+</template> -->
+
+<template>
+    <nav>
+        
+        <p>LOGO</p>
+    
+        <div>
+            <router-link to="/">Pagina inicial</router-link>
+            <router-link to="/about">Sobre nós</router-link>
+        </div>
+    
+        <router-link v-if="link" to="/login">Entrar</router-link>
+        <a v-if="!link" href="/" @click="logout">Sair</a>
+
     </nav>
 </template>
 
 <script>
+    import Cookies from 'js-cookie';
+
     export default{
         name: "Navbar",
+
+        data(){
+            return{
+                link: true
+            }
+        },
+
+        methods: {
+            loadPage(){
+                const token = Cookies.get("_myapp_token");
+
+                !token ? this.link=true : this.link=false ;
+            },
+            logout(){
+                Cookies.remove("_myapp_token");
+            }
+        },
+        mounted() {
+            this.loadPage()
+        }
+
     }
 </script>
 
 <style scoped>
     nav{
+        display: flex;
+        justify-content: space-between;
         background-color: #0b5ed7;
         border-bottom: 2px solid #4388ee;
         padding: 15px 50px;
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
+        
     }
 
-    nav #logo-url{
-        margin: auto;
-        margin-left: 0;
-    }
-
-    #logo{
-        width: 40px;
-        height: 40px;
+    nav p, div{
+        margin-top: 12px;
     }
 
     nav a{
